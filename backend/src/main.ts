ade1a7820e1601a4823dee248bfa46fb49f2f3c9
arifzaidpp <arifzaidaiju@gmail.com>
2025-04-22 17:42:46 +0530

    fix: update CORS configuration to allow all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -66,7 +66,7 @@ async function bootstrap() {
 
   // Enable CORS for all domains
   app.enableCors({
-    origin: "*", // Or specify your frontend URL like 'http://localhost:5173'
+    origin: true, // Or specify your frontend URL like 'http://localhost:5173'
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
